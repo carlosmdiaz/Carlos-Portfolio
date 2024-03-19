9a2992b6900e8c4724ce8cbea125edb158cce58e
carlosmdiaz <carlosdiaz3979@gmail.com>
2024-03-19 16:55:32 -0400

    Last

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -28,7 +28,10 @@ function Skills() {
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
           {imgArr.map((item) => {
             return (
-              <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+              <div
+                key={item.name}
+                className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300"
+              >
                 <div className="grid grid-cols-2 gap-4 justify-center items-center">
                   <div className="m-auto">
                     <Image
